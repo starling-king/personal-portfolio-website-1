8c63d95530beec989e2ef1644860e974b58e48fa
starling-king <jigal6518@gmail.com>
2026-04-07 15:53:42 +0530

    password hashing, and jwt setups

--- models/admin_users.model.js
+++ models/admin_users.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 const adminSchema = new mongoose.Schema({
     username:{
         type:String,
@@ -23,4 +24,39 @@ const adminSchema = new mongoose.Schema({
 
 },{timestamps:true})
 
+adminSchema.pre("save",async function (next) {
+    if(!this.isModified('passwordHash')) {return next();}
+    this.passwordHash = bcrypt.hash(this.passwordHash, 10)
+    next()
+})
+
+adminSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+adminSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+adminSchema.methods.generateesfreshToken = function(){
+    return jwt.sign(
+        {_id:this._id},
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
 export const Admin = mongoose.model("Admin",adminSchema)
+
+
+
