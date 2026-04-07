f26acadba4f58e1093fe3612567dbc172d072331
starling-king <jigal6518@gmail.com>
2026-04-07 16:09:50 +0530

    bug fixed of hashing and jwt

--- models/admin_users.model.js
+++ models/admin_users.model.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+
+//creating modle
 const adminSchema = new mongoose.Schema({
     username:{
         type:String,
@@ -24,15 +26,36 @@ const adminSchema = new mongoose.Schema({
 
 },{timestamps:true})
 
+//bcrypt the password
 adminSchema.pre("save",async function (next) {
     if(!this.isModified('passwordHash')) {return next();}
-    this.passwordHash = bcrypt.hash(this.passwordHash, 10)
+    this.passwordHash = await bcrypt.hash(this.passwordHash, 10)
     next()
 })
 
+//comparing the hash password with user password
 adminSchema.methods.isPasswordCorrect = async function (password){
-    return await bcrypt.compare(password, this.password)
+    return await bcrypt.compare(password, this.passwordHash)
 }
+
+//valadating the env is their or not
+function validateEnv() {
+    const required = [
+        "ACCESS_TOKEN_SECRET",
+        "REFRESH_TOKEN_SECRET",
+        "ACCESS_TOKEN_EXPIRY",
+        "REFRESH_TOKEN_EXPIRY"
+    ];
+
+    required.forEach((key) => {
+        if (!process.env[key]) {
+            throw new Error(`${key} is missing`);
+        }
+    });
+}
+validateEnv();
+
+//generating access token using jwt
 adminSchema.methods.generateAccessToken = function(){
     return jwt.sign({
         _id:this._id,
@@ -45,7 +68,10 @@ adminSchema.methods.generateAccessToken = function(){
     }
 )
 }
-adminSchema.methods.generateesfreshToken = function(){
+
+//generating the refresh token using jwt
+adminSchema.methods.generateRefreshToken = function(){
+
     return jwt.sign(
         {_id:this._id},
         process.env.REFRESH_TOKEN_SECRET,
@@ -55,7 +81,7 @@ adminSchema.methods.generateesfreshToken = function(){
     )
 }
 
-
+//exporting the schema to mongoose modls
 export const Admin = mongoose.model("Admin",adminSchema)
 
 
