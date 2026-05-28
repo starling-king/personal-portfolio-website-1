642347295242b225d742d160d6d8624b80aa0fa2
starling-king <jigal6518@gmail.com>
2026-05-28 11:13:09 +0530

    correcting cors security,and formatting

--- utils/app.js
+++ utils/app.js
@@ -1,22 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
-const app = express()
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
-
-app.use(cors({
-  origin:process.env.CORS_ORIGIN,
-  credentials: true
-}))
-
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
-
-app.use(cookieParser())
+import globalError from "../error/global.error.js";
 
 //routers import
 import createAdminRouter from "../routers/admin_user.router.js"
@@ -25,15 +10,32 @@ import sendMessage from "../routers/contact_messages.router.js"
 import projectRouter from "../routers/Project.router.js"
 import imageRouter from "../routers/project_images.router.js"
 
+const app = express()
+
+const allowedOrigins = process.env.CORS_ORIGIN === "*"
+  ? "*"
+  : process.env.CORS_ORIGIN.split(',');
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}))
+
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
 //router decleration
-app.use("/api/v1/admin",createAdminRouter)
-app.use("/api/v1/content",createSiteContent)
-app.use("/api/v1/Message",sendMessage)
-app.use("/api/v1/Projects",projectRouter)
-app.use("/api/v1/images",imageRouter)
+app.use("/api/v1/admin", createAdminRouter)
+app.use("/api/v1/content", createSiteContent)
+app.use("/api/v1/Message", sendMessage)
+app.use("/api/v1/Projects", projectRouter)
+app.use("/api/v1/images", imageRouter)
 
 //setup global error
-import globalError from "../error/global.error.js";
+
 app.use(globalError);
 
 
